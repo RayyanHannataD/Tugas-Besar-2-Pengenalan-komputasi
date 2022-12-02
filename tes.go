f236d3f3e04f531cf81b49b988ebd81c2babe249
RayyanHannataD <rayyanhd99@gmail.com>
2022-12-02 14:07:32 +0700

    Visualisasi data

---
+++ tes.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+var( 
+mynumber int
+mynumber2 int
+myname string
+)
+func main()  {
+	fmt.Println("tes")
+	mynumber = 20
+	mynumber2 = 40
+	myname = "Rayyan"
+	fmt.Println(myname)
+	fmt.Println(mynumber + mynumber2)
+
+	
+}
